7cb5dc9fd7a96c99c5942d8b639e657b13b9a27f
Armando-Rondon-Hernández <arondonher@educacion.navarra.es>
2021-10-21 12:45:33 +0200

    Version 2

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Desarrollo de Interfaces
+
 ## Proyecto Suite Weather
-Soy Armando Rondón, he desarrollado la app de Weather is Sweet que consiste en poder ver el clima de ese dia de una ciudad o los climas que van hacer los siguentes días. Esta aplicación está desarrollada para ser visualizada en dispositivos móviles
+
+Soy Armando Rondón, he desarrollado la app de Weather is Sweet que consiste en poder ver el clima de ese dia de una ciudad o los climas que van hacer los siguentes días. Esta aplicación está desarrollada para ser visualizada en dispositivos móviles.
+
+Versión 1:
+Solo puedes ver el clima de una ciudad o de sus siguentes días.
+
+Versión 2:
+Mediante un botón puedes ver el clima que hace en tu Localización.
--- www/js/index.js
+++ www/js/index.js
@@ -4,23 +4,25 @@ $(function () {
   $("#buscar").hide();
   $("ul").hide();
   $("#buscarMasDias").hide();
-  $("#toggle").click(function (e) {
-    if (oculto == false) {
-      $("#nav").css("visibility", "visible");
-      oculto = true;
-    } else {
-      $("#nav").css("visibility", "hidden");
-      oculto = false;
-    }
-  });
 });
 
+$("#toggle").click(function (e) {
+  /* if (oculto == false) {
+    $("#navbarToggleExternalContent").className = "";
+    oculto = true;
+  } else {
+    $("#navbarToggleExternalContent").className = "collapse";
+    oculto = false;
+  } */
+  $("#navbarToggleExternalContent").toggleClass("collapse");
+});
 $("#nav-btnBuscar").click(function (e) {
   $("#city").show();
   $("#buscar").show();
   $("#buscarMasDias").show();
   $("#h1").text("Search by city");
   $("h2").hide();
+  $(".card").empty();
 });
 
 //Solo un día
@@ -198,7 +200,7 @@ $("#buscarMasDias").click(function (e) {
               $("#divDeAbajo" + index).append(imagen);
               tituloEstado.innerHTML = "Clouds";
               estado.innerHTML = climas.main.temp + "ºC";
-              $("#" + index).append(tituloEstado);  
+              $("#" + index).append(tituloEstado);
               $("#" + index).append(estado);
               break;
             case "Mist":
@@ -237,3 +239,112 @@ $("#nav-home").click(function (e) {
   $(".card").empty();
   $("ul").empty();
 });
+
+//Geolocalización
+$("#nav-btnLocalizacion").click(function (e) {
+  navigator.geolocation.getCurrentPosition(onSuccess, onError);
+});
+
+var onSuccess = function (position) {
+  $("#h1").text("In Your Location");
+  $("h2").hide();
+  $("#city").hide();
+  $("#buscar").hide();
+  $("ul").hide();
+  $("#buscarMasDias").hide();
+  $(".card").empty();
+  $.get(
+    "https://api.openweathermap.org/data/2.5/weather?units=metric&lat=" +
+      position.coords.latitude +
+      "&lon=" +
+      position.coords.longitude +
+      "&appid=fd7605183e655d8109fff7cb6cdb11ff",
+    function (climaPorCoords) {
+      let nuevoDiv = document.createElement("div");
+      let imagen = document.createElement("img");
+      let estado = document.createElement("p");
+      let tituloEstado = document.createElement("h5");
+      nuevoDiv.className = "card-img-overlay";
+      imagen.className = "card-img ";
+      estado.className = "card-text";
+      tituloEstado.className = "card-title";
+      switch (climaPorCoords.weather[0].main) {
+        case "Thunderstorm":
+          imagen.src = "./img/thunderstrom.jpg";
+          imagen.height = 350;
+          imagen.width = 100;
+          tituloEstado.innerHTML = "Thunderstrom";
+          estado.innerHTML = clima.main.temp + "ºC";
+          $("#divDeAbajo").append(imagen);
+          nuevoDiv.append(tituloEstado);
+          nuevoDiv.append(estado);
+          break;
+        case "Drizzle":
+          imagen.src = "./img/raindrop.jpg";
+          imagen.height = 350;
+          imagen.width = 100;
+          tituloEstado.innerHTML = "Drizzle";
+          estado.innerHTML = climaPorCoords.main.temp + "ºC";
+          $("#divDeAbajo").append(imagen);
+          nuevoDiv.append(tituloEstado);
+          nuevoDiv.append(estado);
+          break;
+        case "Snow":
+          imagen.src = "./img/snow.jpg";
+          imagen.height = 350;
+          imagen.width = 100;
+          tituloEstado.innerHTML = "Snow";
+          estado.innerHTML = climaPorCoords.main.temp + "ºC";
+          $("#divDeAbajo").append(imagen);
+          nuevoDiv.append(tituloEstado);
+          nuevoDiv.append(estado);
+          break;
+        case "Clear":
+          imagen.src = "./img/clear.png";
+          imagen.height = 350;
+          imagen.width = 100;
+          tituloEstado.innerHTML = "Clear";
+          estado.innerHTML = climaPorCoords.main.temp + "ºC";
+          $("#divDeAbajo").append(imagen);
+          nuevoDiv.append(tituloEstado);
+          nuevoDiv.append(estado);
+          break;
+        case "Clouds":
+          imagen.src = "./img/clouds.jpg";
+          imagen.height = 350;
+          imagen.width = 100;
+          $("#divDeAbajo").append(imagen);
+          tituloEstado.innerHTML = "Clouds";
+          estado.innerHTML = climaPorCoords.main.temp + "ºC";
+          nuevoDiv.append(tituloEstado);
+          nuevoDiv.append(estado);
+          break;
+        case "Mist":
+          imagen.src = "./img/misty.jpg";
+          imagen.height = 350;
+          imagen.width = 100;
+          $("#divDeAbajo").append(imagen);
+          tituloEstado.innerHTML = "Mist";
+          estado.innerHTML = climaPorCoords.main.temp + "ºC";
+          nuevoDiv.append(tituloEstado);
+          nuevoDiv.append(estado);
+          break;
+        case "Rain":
+          imagen.src = "./img/raindrop.jpg";
+          imagen.height = 350;
+          imagen.width = 100;
+          $("#divDeAbajo").append(imagen);
+          tituloEstado.innerHTML = "Rain";
+          estado.innerHTML = climaPorCoords.main.temp + "ºC";
+          nuevoDiv.append(tituloEstado);
+          nuevoDiv.append(estado);
+          break;
+      }
+      $("#divDeAbajo").append(nuevoDiv);
+    }
+  );
+};
+
+function onError(error) {
+  console.log("error");
+}
